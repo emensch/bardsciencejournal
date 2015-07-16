c5d8963e9c12a13ebb323bb6a4a2052d02b6e2d4
emensch <eli.mensch@gmail.com>
2015-07-16 03:14:06 -0400

    Update to use mongoose

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express'),
 	env = require('./env.js'),
-	db = require('./db');
+	db = require('./db'),
+	mongoose = require('mongoose'),
+	Post = require('./models/post');
 
 var app = express();
 
-// Setup DB connection and start server
-db.connect(process.env.MONGO_URL, function (err) {
-	if (err) {
-		console.log('Unable to connect to database.\n', err)
-		process.exit(1)
-	} else {
-		 var server = app.listen(3000, function() {
-		 	var port = server.address().port
-			console.log('Listening on port %s...', port)
-		})
-	}
-})
+// Connect to mongodb and run server on success
+mongoose.connect(process.env.MONGO_URL);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error connecting to database:'));
+db.once('open', function () {
+	console.log('Connected to database.');
 
+	var server = app.listen(3000, function() {
+		var port = server.address().port
+		console.log('Listening on port %s...', port)
+	})
+});
