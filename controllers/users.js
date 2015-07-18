2ce2ecb00e62f2c86b79571a47552385a0f900ba
emensch <eli.mensch@gmail.com>
2015-07-17 21:12:22 -0400

    Implement basic user routes & business logic

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,29 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	User = require('../models/user');
 
 router.get('/', function (req, res) {
-	res.send('Users');
+	User.find({}, function(err, users) {
+		if (err) res.sendStatus(500);
+
+		res.json(users);
+	});
+});
+
+router.post('/', function (req, res) {
+	User.createFromBody(req.body, function (err) {
+		if (err) res.sendStatus(500);
+
+		res.sendStatus(201);
+	});
+});
+
+router.delete('/:id', function (req, res) {
+	User.findById(req.params.id).remove( function (err) {
+		if (err) res.sendStatus(500);
+
+		res.sendStatus(200);
+	});
 });
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -1,11 +1,56 @@
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+	Schema = mongoose.Schema,
+	bcrypt = require('bcrypt');
 
 var userSchema = new Schema({
-	username: String, 
-	email: String,
-	password: String,
-	date: { type: Date, default: Date.now }
+	username: { type: String, required: true, index: { unique: true } },
+	email: { type: String, required: true},
+	password: { type: String, required: true},
+	date: { type: Date, default: Date.now },
+	approved: { type: Boolean, default: false }
+
+});
+
+userSchema.statics.createFromBody = function (body, cb) {
+	newUser = new this({
+		username: body.username,
+		email: body.email,
+		password: body.password
+	});
+
+	newUser.save(cb);
+};
+
+userSchema.statics.findByUsername = function (name, cb) {
+	this.find( { username:  name }, function (err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+}
+
+userSchema.methods.cmpPassword = function (pw, cb) {
+	bcrypt.compare(pw, this.password, function (err, match) { 
+		if (err) return cb(err);
+		cb(null, match);
+	});
+};
+
+// Password hashing middleware
+userSchema.pre('save', function (next) { 
+	// Only hash if password has been modified
+	var user = this;
+	if (!user.isModified('password')) return next();
+
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, function (err, hash) {
+			if (err) return next(err);
+
+			user.password = hash;
+			next();
+		});
+	});
 });
 
 module.exports = mongoose.model('User', userSchema);
