50c30b1a87e20d11343c8cf371487dbeef1b3e79
emensch <eli.mensch@gmail.com>
2015-07-18 13:51:53 -0400

    Implement basic type routes & business logic

--- controllers/types.js
+++ controllers/types.js
@@ -1,8 +1,29 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router()
+	Type = require('../models/type');
 
 router.get('/', function (req, res) {
-	res.send('Types');
+	Type.find({}, function(err, types) {
+		if (err) res.sendStatus(500);
+
+		res.json(types);
+	});
 });
 
-module.exports = router
+router.post('/', function (req, res) {
+	Type.createFromBody(req.body, function (err) {
+		if (err) res.sendStatus(500);
+
+		res.sendStatus(201);
+	});
+});
+
+router.delete('/:name', function (req, res) {
+	Type.findByName(req.params.name).remove( function (err) {
+		if (err) res.sendStatus(500);
+
+		res.sendStatus(200);
+	});
+});
+
+module.exports = router;
--- models/type.js
+++ models/type.js
@@ -5,4 +5,16 @@ var typeSchema = new Schema({
 	name: String, 
 });
 
+typeSchema.statics.createFromBody = function (body, cb) {
+	newType = new this({
+		name: body.name
+	});
+
+	newType.save(cb);
+};
+
+typeSchema.statics.findByName = function (name, cb) {
+	return this.find( { name:  name }, cb);
+};
+
 module.exports = mongoose.model('Type', typeSchema);
