11f4dcb05d2cb371afc127d3fd60db15f1f04acd
emensch <eli.mensch@gmail.com>
2015-08-14 05:25:48 -0400

    Add dynamic navbar active class

--- assets/js/navController.js
+++ assets/js/navController.js
@@ -3,5 +3,7 @@ angular
 	.controller('navController', navController);
 
 function navController ($scope, $location) {
-
+	$scope.isActive = function (path) {
+		return path === $location.path();
+	}
 }
