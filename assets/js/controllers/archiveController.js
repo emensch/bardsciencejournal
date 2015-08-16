195e38f63a3db4ea75906bdf2cb2b3dcf2ca398a
emensch <eli.mensch@gmail.com>
2015-08-16 04:54:40 -0400

    Add basic post retrieval to archiveController

--- assets/js/controllers/archiveController.js
+++ assets/js/controllers/archiveController.js
@@ -3,8 +3,25 @@
 		.module('bsj')
 		.controller('archiveController', archiveController);
 
-	function archiveController() {
+	function archiveController($location, postService) {
 		var vm = this;
 		vm.message = 'archive';
+
+		activate();
+
+		function activate() {
+			return getPostWithOptions();
+		}
+
+		function getPostWithOptions() {
+			var options = $location.search();
+
+			return postService.getPosts(options)
+				.then(function(data) {
+					vm.posts = data;
+					console.log(vm.posts);
+					return vm.posts;
+				});
+		}
 	};
 })();
