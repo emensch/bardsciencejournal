c1161875dbfd60abc422fc760d216483ace51189
emensch <eli.mensch@gmail.com>
2015-09-14 16:03:19 -0400

    Add password reset controller

--- assets/admin/js/controllers/resetpassController.js
+++ assets/admin/js/controllers/resetpassController.js
@@ -3,8 +3,38 @@
 		.module('bsj.admin')
 		.controller('resetpassController', resetpassController);
 
-	function resetpassController() {
+	function resetpassController($routeParams, resetpassService) {
 		var vm = this;
+		vm.OK = false;
 
+		var username = $routeParams.username;
+		var token = $routeParams.token;
+
+		activate();
+
+		function activate() {
+			return checkToken(); 
+		}
+
+		function checkToken() {
+			return resetpassService.checkToken(username, token)
+				.then(checkTokenSuccess)
+				.catch(checkTokenFail);
+
+			function checkTokenSuccess() {
+				vm.OK = true;
+			}
+
+			function checkTokenFail() {
+				vm.OK = false;
+			}
+		}
+
+		vm.submitPass = function() {
+			if(vm.pw1 === vm.pw1) {
+				console.log(vm.pw1);
+				resetpassService.submitPass(username, token, vm.pw1);
+			}
+		}
 	};
 })();
