a8180f69f8f96b1bffb5475334617e9a36b94b3b
emensch <eli.mensch@gmail.com>
2015-09-17 01:49:38 -0400

    Build out loginController logic

--- assets/admin/js/controllers/loginController.js
+++ assets/admin/js/controllers/loginController.js
@@ -3,8 +3,30 @@
 		.module('bsj.admin')
 		.controller('loginController', loginController);
 
-	function loginController() {
+	function loginController(authService) {
 		var vm = this;
 
-	};
+		vm.submit = function() {
+			if(vm.username && vm.password) {
+				credentials = {
+					username: vm.username,
+					password: vm.password
+				}
+
+				console.log(credentials);
+
+				return authService.login(credentials)
+					.then(loginComplete)
+					.catch(loginFailed);
+
+				function loginComplete() {
+					console.log(authService.currentUser);
+				}
+
+				function loginFailed() {
+					console.log('Login failed');
+				}
+			}
+		}
+	}
 })();
