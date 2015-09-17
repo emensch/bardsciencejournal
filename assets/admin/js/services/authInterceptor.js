009882ec2945d0c537c26c33de997b8477651373
emensch <eli.mensch@gmail.com>
2015-09-17 01:48:51 -0400

    Add authInterceptor to append creds to XHR

---
+++ assets/admin/js/services/authInterceptor.js
@@ -0,0 +1,28 @@
+(function() {
+	angular
+		.module('bsj.admin')
+		.factory('authInterceptor', authInterceptor);
+
+	function authInterceptor($injector) {
+		var interceptor = {
+			request: appendBasicHeader
+		}
+
+		return interceptor;
+
+		function appendBasicHeader(config) {
+			// Workaround for circular dependency injection
+			var authService = $injector.get('authService');
+			var user = authService.currentUser;
+
+			if(user) {
+				console.log('user logged in');
+				var authStr = getAuthStr(user.username, user.password);
+				config.headers['Authorization'] = authStr;
+				return config; 
+			} 
+
+			return config; 
+		}
+	}
+})();
