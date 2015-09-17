abaf119e568a55ed38b4b24d6374e984db415773
emensch <eli.mensch@gmail.com>
2015-09-17 01:36:38 -0400

    Improve auth middleware to suppress browser login prompt

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,12 +5,8 @@ var User = require('../models/user'),
 passport.use(new BasicStrategy( 
 	function (username, password, cb) {
 		User.findByUsername(username, function (err, user) {
-			if (err) {
-				// Prevent 404 from being returned if user is nonexistent
-				err.http_code = 401;
-				return cb(err);
-			}
 			if (!user) return cb(null, false);
+			if (err) return cb(err);
 			user.checkAuthorized(password, function (err, ok) {
 				if (err) return cb(err);
 				if (!ok) return cb(null, false);
@@ -20,4 +16,26 @@ passport.use(new BasicStrategy(
 	}
 ));
 
-module.exports = passport.authenticate('basic', { session: false });
+module.exports = function (req, res, next) {
+	passport.authenticate('basic', function (err, user, info) {
+		if (err) { 
+			return next(err); 
+		}
+
+		if (!user) {
+			var err = new Error();
+			err.http_code = 401;
+
+			// Suppress default browser login prompt if XHR request
+			if(req.xhr) {
+				res.set('WWW-Authenticate', 'x'+info);
+			}
+
+			return next(err);
+		}
+
+		req.user = user;
+		next();
+
+	})(req, res, next);
+}
