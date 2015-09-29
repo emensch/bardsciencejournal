4c71e86aecbac7c176dcd5ab0705a6a17e795fff
emensch <eli.mensch@gmail.com>
2015-09-29 16:47:54 -0400

    Build out basic admin postController

--- assets/admin/js/controllers/postsController.js
+++ assets/admin/js/controllers/postsController.js
@@ -5,7 +5,25 @@
 		.module('bsj.admin')
 		.controller('postsController', postsController);
 
-	function postsController() {
+	function postsController(postService) {
+		var vm = this;
+
+		vm.posts = [];
+
+		activate();
+
+		function activate() {
+			return getPosts();
+		}
+
+		function getPosts() {
+			return postService.getPosts()
+				.then(function(data) {
+					vm.posts = data;
+					console.log(vm.posts);
+					return vm.posts;
+				});			
+		}
 
 	}
 })();
