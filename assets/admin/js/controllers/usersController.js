b630a30a1259b1deeec4342be1111ebe3dec7c3c
emensch <eli.mensch@gmail.com>
2015-09-30 12:39:48 -0400

    Retrieve and bind users to view

--- assets/admin/js/controllers/usersController.js
+++ assets/admin/js/controllers/usersController.js
@@ -5,7 +5,24 @@
 		.module('bsj.admin')
 		.controller('usersController', usersController);
 
-	function usersController() {
+	function usersController(userService) {
+		var vm = this;
 
+		vm.users = [];
+
+		activate();
+
+		function activate() {
+			return getUsers();
+		}
+
+		function getUsers() {
+			return userService.getUsers()
+				.then(function(data) {
+					vm.users = data;
+					console.log(vm.users);
+					return vm.users;
+				});
+		}
 	}
 })();
