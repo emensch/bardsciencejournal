0c39217e7ffe638b29a6432076d0091bef8d2831
emensch <eli.mensch@gmail.com>
2015-09-30 12:28:36 -0400

    Retrieve and bind descriptors to view

--- assets/admin/js/controllers/descriptorsController.js
+++ assets/admin/js/controllers/descriptorsController.js
@@ -5,7 +5,35 @@
 		.module('bsj.admin')
 		.controller('descriptorsController', descriptorsController);
 
-	function descriptorsController() {
+	function descriptorsController($q, descriptorService) {
+		var vm = this;
 
+		vm.subjects = [];
+		vm.types = [];
+
+		activate();
+
+		function activate() {
+			var promises = [getTypes(), getSubjects()];
+			return $q.all(promises);
+		}
+
+		function getTypes() {
+			return descriptorService.getTypes()
+				.then(function(data) {
+					vm.types = data;
+					console.log(vm.types);
+					return vm.types;
+				});
+		}
+
+		function getSubjects() {
+			return descriptorService.getSubjects()
+				.then(function(data) {
+					vm.subjects = data;
+					console.log(vm.subjects);
+					return vm.subjects;
+				});
+		}
 	}
 })();
