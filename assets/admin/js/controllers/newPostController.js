a2dd31209510e1f951c17dfb95c56c0c5f79080a
emensch <eli.mensch@gmail.com>
2015-10-01 15:17:31 -0400

    Add basic post creation functionality (WIP)

--- assets/admin/js/controllers/newPostController.js
+++ assets/admin/js/controllers/newPostController.js
@@ -3,15 +3,46 @@
 
 	angular
 		.module('bsj.admin')
-		.controller('newPostController', newPostController)
+		.controller('newPostController', newPostController);
 
-	function newPostController(postService) {
+	function newPostController($q, $location, descriptorService, postService) {
 		var vm = this;
 
+		vm.subjects = [];
+		vm.types = [];
 		vm.submitPost = submitPost;
 
-		function submitPost() {
-			
+		activate();
+
+		function activate() {
+			var promises = [getTypes(), getSubjects()];
+			return $q.all(promises);			
+		}
+
+		function getTypes() {
+			return descriptorService.getTypes()
+				.then(function(data) {
+					vm.types = data;
+					console.log(vm.types);
+					return vm.types;
+				});
+		}
+
+		function getSubjects() {
+			return descriptorService.getSubjects()
+				.then(function(data) {
+					vm.subjects = data;
+					console.log(vm.subjects);
+					return vm.subjects;
+				});
+		}
+
+		function submitPost(post) {
+			post.authors = post.authors.split(',').map(function (val){ return val.trim(); });
+			post.tags = post.tags.split(',').map(function (val){ return val.trim(); });
+			console.log(post);
+			$location.path('/admin/posts');
+			postService.createPost(post);
 		}
 	}
 })();
