99ec1c1776a6e7a0911692a083980cbecfcd1df1
emensch <eli.mensch@gmail.com>
2015-10-21 23:40:12 -0400

    Build out dropdown directive

--- assets/blog/js/directives/dropdown.js
+++ assets/blog/js/directives/dropdown.js
@@ -7,15 +7,47 @@
 
 	function dropdown($timeout) {
 		var directive = {
-			link: link,
+			controller: DropdownController,
+			controllerAs: 'vm',
+			bindToController: true,
 			restrict: 'A',
 			require: 'ngModel',
+			scope: {
+				defaultOption: '@',
+				defaultName: '@',
+				items: '='
+			},
+			templateUrl: 'partials/dropdown.html'
 		};
 
 		return directive;
+	}
 
-		function link($scope, $elem, $attrs, ngModel) {
-			
+	function DropdownController($element) {
+		var vm = this;
+
+		vm.select = select;
+		vm.selectDefault = selectDefault;
+		vm.toggleMenu = toggleMenu;
+		vm.active = false;
+		vm.selectedOption = vm.defaultName;
+
+		var ngModel = $element.controller('ngModel');
+
+		function select(item) {
+			ngModel.$setViewValue(item.name);
+			vm.selectedOption = item.name;
+			vm.active = false;
+		}
+
+		function selectDefault() {
+			ngModel.$setViewValue(null);
+			vm.selectedOption = vm.defaultName;
+			vm.active = false;
+		}
+
+		function toggleMenu() {
+			vm.active = !vm.active;
 		}
 	}
 })();
