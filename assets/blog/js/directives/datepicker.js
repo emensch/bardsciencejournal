265a5f9b2703d257ce3e65634ef1c1ceadd74114
emensch <eli.mensch@gmail.com>
2015-10-28 13:48:07 -0400

    Scaffold datepicker directive

---
+++ assets/blog/js/directives/datepicker.js
@@ -0,0 +1,27 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('bsj')
+		.directive('datepicker', datepicker);
+
+	function datepicker() {
+		var directive = {
+			controller: DatepickerController,
+			controllerAs: 'vm',
+			bindToController: true,
+			restrict: 'E',
+			require: 'ngModel',
+			scope: {
+				name: '@',
+			},
+			templateUrl: 'partials/datepicker.html'
+		};
+
+		return directive;
+	}
+
+	function DatepickerController() {
+
+	}
+})();
