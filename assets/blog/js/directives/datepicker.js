5530bfe766b364f138a8166a0d2cc4d223859789
emensch <eli.mensch@gmail.com>
2015-11-04 19:51:09 -0500

    Add date-range support to datepickers

--- assets/blog/js/directives/datepicker.js
+++ assets/blog/js/directives/datepicker.js
@@ -14,7 +14,8 @@
 			require: 'ngModel',
 			scope: {
 				name: '@',
-				defaultDate: '@'
+				minDate: '@',
+				maxDate: '@'
 			},
 			templateUrl: 'partials/datepicker.html'
 		};
@@ -28,28 +29,35 @@
 		vm.toggleMenu = toggleMenu;
 		vm.active = false;
 		vm.activeMonth = activeMonth;
+		vm.disabledMonth = disabledMonth;
 		vm.selectMonth = selectMonth;
-		vm.dateString = vm.defaultDate;
+		vm.dateString = null;
 		vm.nextYear = nextYear;
 		vm.prevYear = prevYear;
 		vm.getYear = getYear;
+		vm.isMinYear = isMinYear;
+		vm.isMaxYear = isMaxYear;
 
 		vm.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 		var inclusive = 'inclusive' in $attrs;
-		var date = new Date();
+		var date = null;
+		var min = null;
+		var max = null;
 
 		var ngModel = $element.controller('ngModel');
 
 		ngModel.$render = function() {
 			if(ngModel.$modelValue) {
-				date = new Date(ngModel.$modelValue);
+				var tmpDate = new Date(ngModel.$modelValue);
+				date = new Date(tmpDate.getFullYear(), tmpDate.getMonth(), 1);
+
+				console.log(ngModel.$modelValue, date);
 				if(inclusive) {
 					date.setMonth(date.getMonth() - 1);
 				}
-				renderDate();
 			} else {
-				date = new Date(vm.defaultDate);
+				date = null;
 			}
 		};
 
@@ -58,41 +66,99 @@
 		}
 
 		function activeMonth(month) {
-			return month === date.getMonth();
+			return date ? month === date.getMonth() : null;
+		}
+
+		function disabledMonth(month) {
+			if(date) {
+				var tmpDate = new Date(date.getFullYear(), month, 1);
+
+				if(min && max) {
+					return !(tmpDate >= min && (inclusive ? tmpDate <= max : tmpDate < max));
+				}
+
+				return false;
+			}
+			return true;
 		}
 
 		function selectMonth(month) {
-			date.setMonth(month);
-			renderDate();
-			vm.active = false;
+			if(!disabledMonth(month)) {
+				vm.active = false;
+				date.setMonth(month);
+				renderDate();			
+			}
 		}
 
 		function nextYear() {
-			date.setYear(date.getFullYear() + 1);
-			renderDate();
+			if(date.getFullYear() < max.getFullYear()) {
+				date.setYear(date.getFullYear() + 1);
+				console.log(date.getMonth(), max.getMonth());
+				if(date.getMonth() >= max.getMonth()) {
+					date.setMonth(inclusive ? max.getMonth() : max.getMonth() - 1);
+				}
+				renderDate();
+			}
 		}
 
 		function prevYear() {
-			date.setYear(date.getFullYear() - 1);
-			renderDate();
+			if(date.getFullYear() > min.getFullYear()) {
+				date.setYear(date.getFullYear() - 1);
+				if(date.getMonth() < min.getMonth()) {
+					date.setMonth(min.getMonth());
+				}
+				renderDate();
+			}
 		}
 
 		function getYear() {
-			return date.getFullYear();
+			return date ? date.getFullYear() : null;
+		}
+
+		function isMinYear() {
+			return (date && min) ? (date.getFullYear() <= min.getFullYear()) : true;
+		}
+
+		function isMaxYear() {
+			return (date && max) ? (date.getFullYear() >= max.getFullYear()) : true;
 		}
 
 		function renderDate() {
-			if(inclusive) {
-				var tmpDate = new Date(date);
-				tmpDate.setMonth(tmpDate.getMonth() + 1);
-
-				ngModel.$setViewValue($filter('date')(tmpDate, 'MM-dd-yyyy'));
+			if(date) {
+				if(inclusive) {
+					var tmpDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+					ngModel.$setViewValue($filter('date')(tmpDate, 'MM-dd-yyyy'));
+				} else {
+					date.setDate(1);
+					ngModel.$setViewValue($filter('date')(date, 'MM-dd-yyyy'));
+				}
+				vm.dateString = $filter('date')(date, 'MMM yyyy');
 			} else {
-				ngModel.$setViewValue($filter('date')(date, 'MM-dd-yyyy'));
+				vm.dateString = '...';
 			}
-
-			vm.dateString = $filter('date')(date, 'MMM yyyy');
 		}
+
+		$scope.$watch('vm.minDate', function() {
+			if(vm.minDate) {
+				if(!inclusive) {
+					date = new Date(vm.minDate);
+					renderDate();
+				}
+				var tmpMin = new Date(vm.minDate);
+				min = new Date(tmpMin.getFullYear(), tmpMin.getMonth(), 1);
+			}
+		});
+
+		$scope.$watch('vm.maxDate', function() {
+			if(vm.maxDate) {
+				if(inclusive) {
+					date = new Date(vm.maxDate);
+					renderDate();				
+				}
+				var tmpMax = new Date(vm.maxDate);
+				max = new Date(tmpMax.getFullYear(), tmpMax.getMonth(), 1);
+			}
+		});
 
 		$element.on('click', clickHandler);
 		function clickHandler(event) {
